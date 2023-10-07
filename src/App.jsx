c82247d7eb6fa8927a58c0608bc9ab6ac7e17609
Nate J.M <nate@natejonah.com>
2023-10-07 23:41:38 +0100

    Create country component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Attr from './components/Attr'
 import './App.css'
+import Country from './components/Country'
 
 function App() {
     return (
@@ -27,46 +28,9 @@ function App() {
                 </div>
 
                 <section>
-                    <article>
-                        <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png" alt="Germany flag" />
-                        <h2>Germany</h2>
-                        <ul>
-                            <li><strong>Population</strong>: <span>81,770,900</span></li>
-                            <li><strong>Region</strong>: <span>Europe</span></li>
-                            <li><strong>Capital</strong>: <span>Berlin</span></li>
-                        </ul>
-                    </article>
-
-                    <article>
-                        <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg/800px-Flag_of_the_United_States_%28DoS_ECA_Color_Standard%29.svg.png" alt="United States of America flag" />
-                        <h2>United States of America</h2>
-                        <ul>
-                            <li><strong>Population</strong>: <span>323,947,000</span></li>
-                            <li><strong>Region</strong>: <span>Americas</span></li>
-                            <li><strong>Capital</strong>: <span>Washington, D.C.</span></li>
-                        </ul>
-                    </article>
-
-                    <article>
-                        <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/720px-Flag_of_Brazil.svg.png?20111003040251" alt="Brazil flag" />
-                        <h2>Brazil</h2>
-                        <ul>
-                            <li><strong>Population</strong>: <span>206,135,893</span></li>
-                            <li><strong>Region</strong>: <span>Americas</span></li>
-                            <li><strong>Capital</strong>: <span>Brasilia</span></li>
-                        </ul>
-                    </article>
-
-                    <article>
-                        <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Iceland.svg/800px-Flag_of_Iceland.svg.png" alt="Iceland flag" />
-                        <h2>Iceland</h2>
-                        <ul>
-                            <li><strong>Population</strong>: <span>334,300</span></li>
-                            <li><strong>Region</strong>: <span>Europe</span></li>
-                            <li><strong>Capital</strong>: <span>Reykjavík</span></li>
-                        </ul>
-                    </article>
+                    <Country />
                 </section>
+
             </main>
 
             <Attr />
---
+++ src/components/Country.jsx
@@ -0,0 +1,13 @@
+export default function Country() {
+    return (
+        <article>
+            <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png" alt="Germany flag" />
+            <h2>Germany</h2>
+            <ul>
+                <li><strong>Population</strong>: <span>81,770,900</span></li>
+                <li><strong>Region</strong>: <span>Europe</span></li>
+                <li><strong>Capital</strong>: <span>Berlin</span></li>
+            </ul>
+        </article>
+    )
+}
