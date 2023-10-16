2ea2f95c7f96384d9e527a1ffac6c4d7f2665ff0
Nate J.M <nate@natejonah.com>
2023-10-16 21:46:19 +0100

    Set up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.jsx'],
+  theme: {
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
