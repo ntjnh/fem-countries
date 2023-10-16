5e106f0dd7c2fc2cc239c66e1c2ba85d0737b7a5
Nate J.M <nate@natejonah.com>
2023-10-16 21:58:55 +0100

    Add colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     fontFamily: {
       sans: ['Nunito Sans', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      colors: {
+        grey: {
+          925: '#111517'
+        },
+        neutral: {
+          450: '#858585'
+        },
+        slate: {
+          750: '#2b3945',
+          775: '#202c37'
+        }
+      }
+    },
   },
   plugins: [],
 }
