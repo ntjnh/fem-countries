67e67170940a810d3cbebbf27e61957d8564435a
Nate J.M <nate@natejonah.com>
2023-10-17 08:08:38 +0100

    Map API data in country component

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,13 +1,19 @@
-export default function Country() {
+export default function Country({ flag, name, population, region, capital }) {
     return (
-        <article>
-            <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1280px-Flag_of_Germany.svg.png" alt="Germany flag" />
-            <h2>Germany</h2>
-            <ul>
-                <li><strong>Population</strong>: <span>81,770,900</span></li>
-                <li><strong>Region</strong>: <span>Europe</span></li>
-                <li><strong>Capital</strong>: <span>Berlin</span></li>
-            </ul>
+        <article style={{
+            "marginBottom": "2rem",
+            "width": "20.7%"
+        }}>
+            <img style={{"maxWidth": "100%"}} src={flag.svg} alt={flag.alt} />
+
+            <div>
+                <h2>{name}</h2>
+                <ul>
+                    <li><strong>Population</strong>: <span>{population}</span></li>
+                    <li><strong>Region</strong>: <span>{region}</span></li>
+                    <li><strong>Capital</strong>: <span>{capital}</span></li>
+                </ul>
+            </div>
         </article>
     )
 }
