4adf299bad6efa8ce0f746230dd3ebd4c18b4ad5
Nate J.M <nate@natejonah.com>
2023-10-17 08:08:58 +0100

    Set up variables for API data in country info component

--- src/components/CountryInfo.jsx
+++ src/components/CountryInfo.jsx
@@ -1,33 +1,60 @@
-export default function CountryInfo() {
+export default function CountryInfo(props) {
+    const popup = {
+        position: "absolute",
+        top: "24px",
+        padding: "80px 0",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "rgba(255, 255, 255, 0.9)",
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        alignItems: "center",
+    }
+
     return (
-        <article>
-            <div>
-                <img style={{"maxWidth": "266px"}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Belgium.svg/800px-Flag_of_Belgium.svg.png" alt="Germany flag" />
+        <article 
+            style={popup}
+        >
+    
+            <div style={{width: "49%"}}>
+                <img style={{"maxWidth": "266px"}} src={props.flag} alt={props.flagAlt} />
             </div>
 
-            <div>
-                <h2>Germany</h2>
+            <div style={{width: "49%"}}>
+                <h2>{props.name}</h2>
 
                 <ul>
-                    <li><strong>Native Name:</strong> <span>België</span></li>
-                    <li><strong>Population:</strong> <span>11,319,511</span></li>
-                    <li><strong>Region:</strong> <span>Europe</span></li>
-                    <li><strong>Sub Region:</strong> <span>Western Europe</span></li>
-                    <li><strong>Capital:</strong> <span>Brussels</span></li>
+                    <li><strong>Native Name:</strong> <span>{props.nameNative}</span></li>
+                    <li><strong>Population:</strong> <span>{props.population}</span></li>
+                    <li><strong>Region:</strong> <span>{props.region}</span></li>
+                    <li><strong>Sub Region:</strong> <span>{props.subregion}</span></li>
+                    <li><strong>Capital:</strong> <span>{props.capital}</span></li>
                 </ul>
 
                 <ul>
-                    <li><strong>Top Level Domain:</strong> <span>.be</span></li>
-                    <li><strong>Currencies:</strong> <span>Euro</span></li>
-                    <li><strong>Languages:</strong> <span>Dutch, French, German</span></li>
+                    <li>
+                        <strong>Top Level Domain:</strong> <span>{props.tld}</span>
+                    </li>
+                    <li>
+                        <strong>Currencies:</strong> 
+                        
+                        <ul>
+                            {props.currencies}
+                        </ul>
+                    </li>
+                    <li>
+                        <strong>Languages:</strong> 
+                        <ul>
+                            {props.languages}
+                        </ul>
+                    </li>
                 </ul>
 
                 <h3>Border Countries:</h3>
 
                 <ul>
-                    <li>France</li>
-                    <li>Germany</li>
-                    <li>Netherlands</li>
+                    {props.languages}
                 </ul>
             </div>
         </article>
