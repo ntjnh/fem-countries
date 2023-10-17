13241cceea93cf6af5adec91b0c2b0bebbc8f53f
Nate J.M <nate@natejonah.com>
2023-10-17 22:00:34 +0100

    Add spacing to header and style heading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import { faMoon } from '@fortawesome/free-regular-svg-icons'
 
 export default function Header() {
     return (
-        <header>
-            <div className="container mx-auto">
-                <h1>Where in the world?</h1>
+        <header className="py-6">
+            <div className="container flex items-center justify-between mx-auto">
+                <h1 className="font-bold m-0 text-2xl">Where in the world?</h1>
 
                 <button>
                     <FontAwesomeIcon icon={faMoon} />
