33196dc8b8903b28530b6786f24e7db81fce41b4
Nate J.M <nate@natejonah.com>
2023-10-17 22:00:18 +0100

    Add classes to set form layout

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,15 @@
 export default function Form() {
     return (
-        <div
-            style={{"marginBottom": "1.5rem"}}
-        >
-            <input type="text" name="" id="" placeholder="Search for a country..." />
+        <div className="flex items-center justify-between mb-12">
+            <input className="px-8 py-5 rounded-lg text-sm w-[37.5%]" type="text" name="" id="" placeholder="Search for a country..." />
 
             <select name="" id="">
-                <option>Filter by Region</option>
-                <option value="africa">Africa</option>
-                <option value="america">Americas</option>
-                <option value="asia">Asia</option>
-                <option value="europe">Europe</option>
-                <option value="oceania">Oceania</option>
+                <option className="text-sm">Filter by Region</option>
+                <option className="text-sm" value="africa">Africa</option>
+                <option className="text-sm" value="america">Americas</option>
+                <option className="text-sm" value="asia">Asia</option>
+                <option className="text-sm" value="europe">Europe</option>
+                <option className="text-sm" value="oceania">Oceania</option>
             </select>
         </div>
     )
