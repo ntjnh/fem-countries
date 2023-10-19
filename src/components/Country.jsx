133fa9140d5725dc4bf50e9abc0f892968e1bca0
Nate J.M <nate@natejonah.com>
2023-10-19 08:38:47 +0100

    Tweak styles for country component

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,14 +1,21 @@
 export default function Country({ flag, name, population, region, capital }) {
     return (
-        <article className="bg-white mb-8 rounded-lg">
-            <img className="max-w-full rounded-t-lg" src={flag.svg} alt={flag.alt} />
+        <article className="bg-white mb-0 rounded-lg shadow-lg shadow-neutral-100">
+            <div 
+                className="aspect-[16/10] bg-center bg-cover bg-no-repeat overflow-hidden rounded-t-lg"
+                style={{
+                    backgroundImage: `url('${flag.svg}')`
+                }}
+            >
+                
+            </div>
 
-            <div className="px-6 py-8">
-                <h2 className="font-extrabold text-base">{name}</h2>
+            <div className="px-6 py-6">
+                <h2 className="font-extrabold mb-4 text-lg">{name}</h2>
                 <ul className="text-sm">
-                    <li><strong>Population</strong>: <span>{population}</span></li>
-                    <li><strong>Region</strong>: <span>{region}</span></li>
-                    <li><strong>Capital</strong>: <span>{capital}</span></li>
+                    <li className="mb-1"><span className="font-semibold">Population</span>: <span className="font-light">{population}</span></li>
+                    <li className="mb-1"><span className="font-semibold">Region</span>: <span className="font-light">{region}</span></li>
+                    <li><span className="font-semibold">Capital</span>: <span className="font-light">{capital}</span></li>
                 </ul>
             </div>
         </article>
