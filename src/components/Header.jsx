742485948e2eae454ded721fff21507dd9d01cab
Nate J.M <nate@natejonah.com>
2023-10-21 14:46:14 +0100

    Header component mobile tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,17 @@ import { faMoon } from '@fortawesome/free-regular-svg-icons'
 import { faMoon as faMoonFilled } from '@fortawesome/free-solid-svg-icons'
 
 export default function Header({ isDark, modeToggle }) {
-    const lightClasses = 'bg-white shadow-md shadow-neutral-200/60'
+    const lightClasses = 'bg-white shadow-md shadow-neutral-200/60 text-grey-925'
     const darkClasses = 'bg-slate-750 text-white'
     const modeClasses = isDark ? darkClasses : lightClasses 
 
     return (
-        <header className={`py-6 ${modeClasses}`}>
+        <header className={`p-7 md:py-6 ${modeClasses}`}>
             <div className="container flex items-center justify-between mx-auto">
-                <h1 className="font-bold m-0 text-2xl text-grey-925">Where in the world?</h1>
+                <h1 className="font-bold m-0 text-sm md:text-lg lg:text-2xl">Where in the world?</h1>
 
-                <button onClick={() => modeToggle()}>
-                    <FontAwesomeIcon className="mr-3 text-grey-925" icon={isDark ? faMoonFilled : faMoon} />
+                <button className="text-xs md:text-sm lg:text-base" onClick={() => modeToggle()}>
+                    <FontAwesomeIcon className="mr-3" icon={isDark ? faMoonFilled : faMoon} />
                     Dark Mode
                 </button>
             </div>
