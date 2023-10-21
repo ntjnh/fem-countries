eb3d12451d253aa8927a670cd1b2eb5005dc55f4
Nate J.M <nate@natejonah.com>
2023-10-21 14:39:35 +0100

    Add some basic styling for Attr component

--- src/components/Attr.jsx
+++ src/components/Attr.jsx
@@ -1,9 +1,10 @@
 export default function Attr() {
     return (
-        <footer className="attribution">
-            <div>
-                Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a><br />
-                Coded by <a href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>
+        <footer className="attribution bg-white mt-12 p-6 text-center">
+            <div className="text-grey-925 text-sm">
+                Challenge by <a className="hover:underline text-green-700" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>
+                <span className="mx-2">|</span>
+                Coded by <a className="hover:underline text-green-700" href="https://natejonah.com" target="_blank" rel="noreferrer">Nate</a>
             </div>
         </footer>
     )
