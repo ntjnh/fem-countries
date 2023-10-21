4c294a2ded7a33ba47ca6507aa30fca2ad8538be
Nate J.M <nate@natejonah.com>
2023-10-21 15:30:43 +0100

    Mobile tweaks on Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,23 +3,25 @@ import { faMagnifyingGlass, faChevronDown } from '@fortawesome/free-solid-svg-ic
 
 export default function Form({ isDark, onChange, searchTerm }) {
     const lightClasses = {
-        icon: 'text-neutral-450',
+        inputIcon: 'text-neutral-450',
         input: 'shadow-md shadow-neutral-200/60 text-neutral-450',
-        select: 'text-neutral-450 shadow-md shadow-neutral-200/60 text-grey-925'
+        select: 'shadow-md shadow-neutral-200/60 text-grey-925',
+        selectIcon: 'text-grey-925'
     }
     const darkClasses = {
-        icon: 'text-white',
+        inputIcon: 'text-white',
         input: 'bg-slate-750 placeholder:text-white shadow-lg shadow-slate-800 text-white',
-        select: 'bg-slate-750 shadow-lg shadow-slate-800 text-white'
+        select: 'bg-slate-750 shadow-lg shadow-slate-800 text-white',
+        selectIcon: 'text-white'
     }
 
     const modeClasses = isDark ? darkClasses : lightClasses
 
     return (
-        <div className="flex items-center justify-between mb-12 relative">
-            <div className="country-search relative w-[37.5%]">
+        <div className="md:flex md:items-center md:justify-between mb-7 lg:mb-12">
+            <div className="country-search mb-9 md:mb-0 relative md:w-[37.5%]">
                 <FontAwesomeIcon 
-                    className={`absolute left-7 text-md top-5 z-10 ${modeClasses.icon}`} 
+                    className={`absolute left-7 text-md top-5 z-10 ${modeClasses.inputIcon}`} 
                     icon={faMagnifyingGlass}
                 />
                 <input 
@@ -34,22 +36,24 @@ export default function Form({ isDark, onChange, searchTerm }) {
                 <p className="absolute">{searchTerm}</p>
             </div>
 
-            <select 
-                className={`appearance-none rounded-lg min-w-[200px] pl-6 pr-16 py-5 text-sm ${modeClasses.select}`}
-                name="regionFilter" 
-                id="regionFilter"
-            >
-                <option className="text-sm">Filter by Region</option>
-                <option className="text-sm" value="africa">Africa</option>
-                <option className="text-sm" value="america">Americas</option>
-                <option className="text-sm" value="asia">Asia</option>
-                <option className="text-sm" value="europe">Europe</option>
-                <option className="text-sm" value="oceania">Oceania</option>
-            </select>
-            <FontAwesomeIcon 
-                className={`absolute right-4 text-grey-925 text-xs z-10 ${modeClasses.icon}`} 
-                icon={faChevronDown}
-            />
+            <div className="relative w-[200px]">
+                <select 
+                    className={`appearance-none rounded-lg pl-6 pr-8 py-[17px] lg:py-5 text-xs lg:text-sm w-full ${modeClasses.select}`}
+                    name="regionFilter" 
+                    id="regionFilter"
+                >
+                    <option className="text-sm">Filter by Region</option>
+                    <option className="text-sm" value="africa">Africa</option>
+                    <option className="text-sm" value="america">Americas</option>
+                    <option className="text-sm" value="asia">Asia</option>
+                    <option className="text-sm" value="europe">Europe</option>
+                    <option className="text-sm" value="oceania">Oceania</option>
+                </select>
+                <FontAwesomeIcon 
+                    className={`absolute right-4 text-xs top-5 z-10 ${modeClasses.selectIcon}`} 
+                    icon={faChevronDown}
+                />
+            </div>
         </div>
     )
 }
