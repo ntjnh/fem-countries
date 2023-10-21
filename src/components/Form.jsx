8b06cd9cb4ced2142bc51a314cba0cc2fcb4c3a9
Nate J.M <nate@natejonah.com>
2023-10-21 14:39:53 +0100

    Finish basic form component styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function Form({ isDark, onChange, searchTerm }) {
     const lightClasses = {
         icon: 'text-neutral-450',
         input: 'shadow-md shadow-neutral-200/60 text-neutral-450',
-        select: 'text-neutral-450 shadow-md shadow-neutral-200/60'
+        select: 'text-neutral-450 shadow-md shadow-neutral-200/60 text-grey-925'
     }
     const darkClasses = {
         icon: 'text-white',
@@ -16,11 +16,14 @@ export default function Form({ isDark, onChange, searchTerm }) {
     const modeClasses = isDark ? darkClasses : lightClasses
 
     return (
-        <div className="flex items-center justify-between mb-12">
+        <div className="flex items-center justify-between mb-12 relative">
             <div className="country-search relative w-[37.5%]">
-                <FontAwesomeIcon className={`absolute left-7 text-md top-5 z-10 ${modeClasses.icon}`} icon={faMagnifyingGlass} />
+                <FontAwesomeIcon 
+                    className={`absolute left-7 text-md top-5 z-10 ${modeClasses.icon}`} 
+                    icon={faMagnifyingGlass}
+                />
                 <input 
-                    className={`font-light pl-16 pr-8 py-[18px] relative rounded-lg text-sm w-full ${modeClasses.input}`}
+                    className={`font-light pl-16 pr-8 py-[18px] rounded-lg text-sm w-full ${modeClasses.input}`}
                     type="text" 
                     name="countrySearch" 
                     id="countrySearch" 
@@ -28,11 +31,11 @@ export default function Form({ isDark, onChange, searchTerm }) {
                     onChange={onChange}
                     value={searchTerm}
                 />
-                <p className='absolute'>{searchTerm}</p>
+                <p className="absolute">{searchTerm}</p>
             </div>
 
             <select 
-                className={`rounded-lg p-4 text-sm ${modeClasses.select}`}
+                className={`appearance-none rounded-lg min-w-[200px] pl-6 pr-16 py-5 text-sm ${modeClasses.select}`}
                 name="regionFilter" 
                 id="regionFilter"
             >
@@ -43,6 +46,10 @@ export default function Form({ isDark, onChange, searchTerm }) {
                 <option className="text-sm" value="europe">Europe</option>
                 <option className="text-sm" value="oceania">Oceania</option>
             </select>
+            <FontAwesomeIcon 
+                className={`absolute right-4 text-grey-925 text-xs z-10 ${modeClasses.icon}`} 
+                icon={faChevronDown}
+            />
         </div>
     )
 }
